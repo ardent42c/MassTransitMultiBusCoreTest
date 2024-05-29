411d707871fb3a6f5ab05dbbb1401237ab27d492
Alon Gingold <alon.gingold@gmail.com>
2024-05-30 00:50:38 +0300

    Add extension for adding bus by name instead of type

---
+++ MultiBusTestCore/MassTransitExtensions.cs
@@ -0,0 +1,113 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace MultiBusTest
+{
+    public static class MassTransitExtensions
+    {
+
+        public static IBus? GetMassTransitBus(this IServiceProvider serviceProvider, string serverName)
+        {
+            var serverNum = GetServerNum(serverName);
+            if (serverNum < 0)
+                return null;
+            switch (serverNum)
+            {
+                case 0:
+                    return serviceProvider.GetService<IBus>();
+                case 1:
+                    return serviceProvider.GetService<IBus1>();
+                case 2:
+                    return serviceProvider.GetService<IBus2>();
+                case 3:
+                    return serviceProvider.GetService<IBus3>();
+                case 4:
+                    return serviceProvider.GetService<IBus4>();
+                case 5:
+                    return serviceProvider.GetService<IBus5>();
+                case 6:
+                    return serviceProvider.GetService<IBus6>();
+                case 7:
+                    return serviceProvider.GetService<IBus7>();
+                case 8:
+                    return serviceProvider.GetService<IBus8>();
+                case 9:
+                    return serviceProvider.GetService<IBus9>();
+            }
+            return null;
+        }
+
+        public static IServiceCollection AddMassTransit(this IServiceCollection collection, string serverName, object? serverData,
+            Action<IBusRegistrationConfigurator, string, object> configure)
+        {
+            var serverNum = GetServerNum(serverName, true);
+            switch (serverNum)
+            {
+                case 0:
+                    collection.AddMassTransit(cfg => configure(cfg, serverName, serverData));
+                    break;
+                case 1:
+                    collection.AddMassTransit<IBus1>(cfg => configure(cfg, serverName, serverData));
+                    break;
+                case 2:
+                    collection.AddMassTransit<IBus2>(cfg => configure(cfg, serverName, serverData));
+                    break;
+                case 3:
+                    collection.AddMassTransit<IBus3>(cfg => configure(cfg, serverName, serverData));
+                    break;
+                case 4:
+                    collection.AddMassTransit<IBus4>(cfg => configure(cfg, serverName, serverData));
+                    break;
+                case 5:
+                    collection.AddMassTransit<IBus5>(cfg => configure(cfg, serverName, serverData));
+                    break;
+                case 6:
+                    collection.AddMassTransit<IBus6>(cfg => configure(cfg, serverName, serverData));
+                    break;
+                case 7:
+                    collection.AddMassTransit<IBus7>(cfg => configure(cfg, serverName, serverData));
+                    break;
+                case 8:
+                    collection.AddMassTransit<IBus8>(cfg => configure(cfg, serverName, serverData));
+                    break;
+                case 9:
+                    collection.AddMassTransit<IBus9>(cfg => configure(cfg, serverName, serverData));
+                    break;
+            }
+            return collection;
+        }
+
+
+        private static int GetServerNum(string serverName, bool makeNew = false)
+        {
+            serverName = serverName.ToUpperInvariant();
+            if (BusNamesToNum.TryGetValue(serverName, out int serverNum))
+                return serverNum;
+            if (!makeNew)
+                return -1;
+            serverNum = Interlocked.Increment(ref _LastServerNum);
+            if (serverNum >= _MaxBusses)
+                return -1;
+            BusNamesToNum[serverName] = serverNum;
+            return serverNum;
+        }
+
+        private static ConcurrentDictionary<string, int> BusNamesToNum = new ConcurrentDictionary<string, int>();
+        private static int _LastServerNum = -1;
+        private const int _MaxBusses = 10;
+
+    }
+
+    public interface IBus1 : IBus { };
+    public interface IBus2 : IBus { };
+    public interface IBus3 : IBus { };
+    public interface IBus4 : IBus { };
+    public interface IBus5 : IBus { };
+    public interface IBus6 : IBus { };
+    public interface IBus7 : IBus { };
+    public interface IBus8 : IBus { };
+    public interface IBus9 : IBus { };
+}
--- MultiBusTestCore/Program.cs
+++ MultiBusTestCore/Program.cs
@@ -1,5 +1,7 @@
 ﻿using MassTransit;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,7 @@ namespace MultiBusTest
     internal class Program
     {
 
-
+        // this example uses the same region, accessKey and secretKey for all servers, but obviously, each server can get different ones.
         private static readonly string region = "eu-central-1"; // Set this!! 
         private static readonly string accessKey = "mykey";    // Set this!!
         private static readonly string secretKey = "mysecret"; // Set this!!
@@ -21,86 +23,67 @@ namespace MultiBusTest
         static async Task Main(string[] args)
         {
             var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging();
 
+            var servers = new string[] { "server1", "server2", "server3" };
 
-            serviceCollection.AddMassTransit(busRegistrationConfigurator =>
+            foreach (var server in servers)
             {
-                string serverName = "server1";
-
-                //busRegistrationConfigurator.SetEndpointNameFormatter(
-                //    new KebabCaseEndpointNameFormatter(prefix: serverName, includeNamespace: false)); // this might not be needed as we set endpoint name directly.
-                busRegistrationConfigurator
-                    .AddConsumer<MyMessageConsumer>(cfg =>
-                    {
-                        cfg.UseConcurrencyLimit(1);
-                    }); /*.Endpoint(c =>
-                    {
-                        c.Name = $"{serverName}-queue";
-                    });*/
-                busRegistrationConfigurator.UsingAmazonSqs((context, sqsBusFactoryConfigurator) =>
+                // serverName is used to register one of the IBusX types to this name and is also sent to the bus configurator.
+                // serverData can be used to pass any data to lazy called bus configurator.
+                serviceCollection.AddMassTransit(serverName: server, serverData: null, 
+                    (busRegistrationConfigurator, serverName, data) =>
                 {
-                    sqsBusFactoryConfigurator.Host(region, h =>
+                    busRegistrationConfigurator
+                        .AddConsumer<MyMessageConsumer>(cfg =>
+                        {
+                            cfg.UseConcurrencyLimit(1);
+                        }); 
+                    busRegistrationConfigurator.UsingAmazonSqs((context, sqsBusFactoryConfigurator) =>
                     {
-                        h.AccessKey(accessKey);
-                        h.SecretKey(secretKey);
+                        sqsBusFactoryConfigurator.LocalstackHost();
+                        sqsBusFactoryConfigurator.Host(region, h =>
+                        {
+                            h.AccessKey(accessKey);
+                            h.SecretKey(secretKey);
+                        });
+                        var nameFormatter = new BusEnvironmentNameFormatter(
+                            sqsBusFactoryConfigurator.MessageTopology.EntityNameFormatter, serverName);
+                        sqsBusFactoryConfigurator.MessageTopology.SetEntityNameFormatter(nameFormatter);
+                        sqsBusFactoryConfigurator.ConfigureEndpoints(context,
+                            new KebabCaseEndpointNameFormatter(prefix: $"{serverName}-queue", includeNamespace: false));
                     });
-                    var nameFormatter = new BusEnvironmentNameFormatter(
-                        sqsBusFactoryConfigurator.MessageTopology.EntityNameFormatter, serverName);
-                    sqsBusFactoryConfigurator.MessageTopology.SetEntityNameFormatter(nameFormatter);
-                    sqsBusFactoryConfigurator.ConfigureEndpoints(context,
-                        new KebabCaseEndpointNameFormatter(prefix: serverName, includeNamespace: false));
                 });
-            } );
-
-            // this code is an exact copy of the above code. Only changes are the <IBus2> and the changed serverName.
-            // we are using the SAME consumer of MyMessageConsumer.
-            serviceCollection.AddMassTransit<ISecondBus>(busRegistrationConfigurator =>
-            {
-                var server2Name = "server2";
-                //busRegistrationConfigurator.SetEndpointNameFormatter(
-                //    new KebabCaseEndpointNameFormatter(prefix: server2Name, includeNamespace: false)); // this might not be needed as we set endpoint name directly.
-                busRegistrationConfigurator
-                    .AddConsumer<MyMessageConsumer>(cfg =>
-                    {
-                        cfg.UseConcurrencyLimit(1);
-                    });/*.Endpoint(c =>
-                    {
-                        c.Name = $"{server2Name}-queue";
-                    });*/
-
-                busRegistrationConfigurator.UsingAmazonSqs((context, sqsBusFactoryConfigurator) =>
-                {
-                    sqsBusFactoryConfigurator.Host(region, h =>
-                    {
-                        h.AccessKey(accessKey);
-                        h.SecretKey(secretKey);
-                    });
-                    var nameFormatter = new BusEnvironmentNameFormatter(
-                        sqsBusFactoryConfigurator.MessageTopology.EntityNameFormatter, server2Name);
-                    sqsBusFactoryConfigurator.MessageTopology.SetEntityNameFormatter(nameFormatter);
-                    sqsBusFactoryConfigurator.ConfigureEndpoints(context,
-                        new KebabCaseEndpointNameFormatter(prefix: server2Name, includeNamespace: false)
-                        );
-                });
-            });
+            }
 
             var sp = serviceCollection.BuildServiceProvider();
-            var GlobalBusController = sp.GetService<IBusControl>()!;
-            var bus1 = sp.GetService<IBus>();
-            var bus2 = sp.GetService<ISecondBus>();
-            await GlobalBusController.StartAsync();
+            List<IBus> busses = new List<IBus>();
+            foreach (var server in servers)
+            {
+                var bus = sp.GetMassTransitBus(server);
+                if (bus != null)
+                    busses.Add(bus);
+            }
+            var hostedServices = sp.GetServices<IHostedService>();
+            var cancelToken = new CancellationToken();
+            foreach (var service in hostedServices)
+            {
+                Console.WriteLine($"starting service {service.GetType().Name}");
+                await service.StartAsync(cancelToken);
+            }
+
             Console.Write("Started servers. Press Enter to shut down.");
             Console.ReadLine();
-            await GlobalBusController.StopAsync();
-            // What SHOULD have been created:
+            foreach (var service in hostedServices)
+                await service.StopAsync(cancelToken);
+
+            // What SHOULD be created (now works):
             //      Topic: server1-topic-MyMessage
             //      Topic: server2-topic-MyMessage
-            //      Queue: server1-queue (with subscription to server1-topic-MyMessage)
-            //      Queue: server2-queue (with subscription to server2-topic-MyMessage)
-            
-            // What was ACTUALLY created:
-            //      Topic: server1-topic-MyMessage
-            //      Queue: server2-queue (with subscription to server1-topic-MyMessage)
+            //      Topic: server3-topic-MyMessage
+            //      Queue: server1-queue-MyMessage (with subscription to server1-topic-MyMessage)
+            //      Queue: server2-queue-MyMessage (with subscription to server2-topic-MyMessage)
+            //      Queue: server4-queue-MyMessage (with subscription to server3-topic-MyMessage)
         }
     }
 }
